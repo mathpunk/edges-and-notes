a48237fbf6bc40afc81d3f4349aaa19af778b78b
Thomas Henderson <tom@mathpunk.net>
2014-08-13 14:26:19 -0700

    not broken yet

--- src/edges_and_notes/server/handler.clj
+++ src/edges_and_notes/server/handler.clj
@@ -4,33 +4,14 @@
             [compojure.route :as route]
             [clojure.data.json :as json]))
 
-(defn bad-alchemy [data]
-  (str
-  "<html>
-    <head>
-      <link rel=\"stylesheet\" href=\"http://cdn.graphalchemist.com/alchemy.min.css\">
-      <script type=\"text/javascript\" src=\"http://cdn.graphalchemist.com/alchemy.min.js\"></script>
-    </head>
-    <body>
-      <div id=\"alchemy\" class=\"alchemy\"></div>
-  <script>
-    var some_data = "
+(def test-data
+     {:nodes
+      [ {:id 1 :caption "wowow"} {:id 2 :caption "zowie"} {:id 3 :caption "whatWHAT"}]
+      :edges
+      [ {:source 1 :target 2 :caption "edgez"}
+         {:source 1 :target 3 :caption "another edgez"} ]})
 
-   data
-   ;; it's a string of json
-   ;; wtf
-   ;; don't forget the semicolon
-   ;; and the escaped quotes
-   ;; ohgod
-   ";
-   alchemy.begin({\"dataSource\": some_data})
-</script>
-
-  </body>
-</html>" ))
-
-
-(defn alchemy-page [data]
+(defn alchemy-full-page [data]
   (str
   "<html>
     <head>
@@ -43,17 +24,10 @@
     </body>
   </html>"))
 
-(def test-data
-     {:nodes
-      [ {:id 1 :caption "wowow"} {:id 2 :caption "zowie"} {:id 3 :caption "whatWHAT"}]
-      :edges
-      [ {:source 1 :target 2 :caption "edgez"}
-         {:source 1 :target 3 :caption "another edgez"} ]})
-
 (defroutes app-routes
   (GET "/" [] "oh hai i'm teh internet")
-  (GET "/data" [] (alchemy-page test-data))
-  (route/resources "/")                          ;; These two routes came with the new Compojure project.
+  (GET "/test/full-page" [] (alchemy-full-page test-data))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
